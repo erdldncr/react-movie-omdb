b2a8217b22cc93759c50d5ce06cb1b6bef92a1d9
erdldncr <erdldncr@gmail.com>
2021-06-14 13:59:12 +0100

    ccomplete project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import AddFavoruites from './components/AddFavoruites'
 import MovieList from "./components/MovieList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -10,14 +10,16 @@ import SearchBox from "./components/SearchBox";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("star");
+  const [favourites,setFavourites]=useState([])
   const getMovieRequest = async () => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=785556f3`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    setMovies(responseJson.Search);
+    
+    responseJson.Search&&setMovies(responseJson.Search);
   };
   useEffect(() => {
     getMovieRequest();
@@ -25,15 +27,25 @@ function App() {
   const handleSearch = (e) => {
     setSearchValue(e.target.value);
   };
-
+  const handleFavouritesClick=(movie)=>{
+    const newFavList=[...favourites,movie]
+    setFavourites(newFavList)
+  }
+  console.log(favourites)
   return (
     <div className="container-fluid movie-app">
-      <div className="row">
+      <div className="row d-flex align-items-center mt-4 mb-4">
         <MovieListHeading heading="Movies" />
         <SearchBox handleSearch={handleSearch} searchValue={searchValue} />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList movies={movies} handleFavouritesClick={handleFavouritesClick} favoruiteComponent={AddFavoruites} />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favorutie Movies" />
+        <div className="row">
+        <MovieList movies={favourites} handleFavouritesClick={handleFavouritesClick} favoruiteComponent={AddFavoruites} />
+      </div>
       </div>
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-export default function MovieList({movies}) {
+
+export default function MovieList(props) {
+    const FavoutiteComponent=props.favoruiteComponent
+    const handleFavouritesClick=props.handleFavouritesClick
     return (
         <>
-         {movies.map(movie=>(
-             <div className="d-flex justify-content-start m-3">
+         {props.movies?.map((movie,idx)=>(
+             <div  key={movie.idx} className="d-flex justify-content-start m-3 image-container">
                      <img src={movie.Poster} alt="moviePoster"/>
+                 <div 
+                 onClick={()=>handleFavouritesClick(movie)}
+                 className="overlay d-flex align-items-center justify-content-center">
+                   <FavoutiteComponent  />
+                     </div>    
              </div>
          ))}   
         </>
